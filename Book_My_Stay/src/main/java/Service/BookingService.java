efebb222355524f299efcacbdd85733fecb0d083
AbinayaSelvam <sabinaya352@gmail.com>
2026-06-19 17:24:56 +0530

    added the booking process

--- Book_My_Stay/src/main/java/Service/BookingService.java
+++ Book_My_Stay/src/main/java/Service/BookingService.java
@@ -1,4 +1,107 @@
 package Service;
+import Entity.Customer;
+import Entity.Reservation;
+import Entity.Service;
+
+import java.util.*;
 
 public class BookingService {
+    private Queue<Reservation> bookingQueue = new LinkedList<>();
+    private Set<String> allocatedRooms = new HashSet<>();
+    private Map<String, List<Service>> reservationServices = new HashMap<>();
+    private int reservationCounter = 1;
+
+    public Reservation createBooking(
+            String customerName,
+            String roomType) {
+
+        Reservation reservation =
+                new Reservation(
+
+                        "R" +
+                                reservationCounter++,
+
+                        new Customer(
+                                customerName
+                        ),
+
+                        roomType
+                );
+
+        bookingQueue.offer(
+                reservation
+        );
+
+        return reservation;
+    }
+
+    public void processBooking(
+            InventoryService inventory,
+            HistoryService history) {
+
+        if (bookingQueue.isEmpty()) {
+
+            System.out.println(
+                    "No Pending Bookings"
+            );
+
+            return;
+        }
+
+        Reservation reservation =
+                bookingQueue.poll();
+
+        if (!inventory.isAvailable(
+                reservation.getRoomType()
+        )) {
+
+            System.out.println(
+                    "Room Not Available"
+            );
+
+            return;
+        }
+
+        String roomId =
+
+                reservation.getRoomType()
+                        .substring(0, 1)
+                        .toUpperCase()
+
+                        +
+
+                        (100 +
+                                allocatedRooms.size());
+
+        allocatedRooms.add(
+                roomId
+        );
+
+        reservation.setRoomId(
+                roomId
+        );
+
+        inventory.reduceRoomCount(
+                reservation.getRoomType()
+        );
+
+//        history.addHistory(
+//                reservation
+//        );
+
+        System.out.println(
+
+                "\nBooking Confirmed"
+                        +
+                        "\nReservation ID : "
+                        +
+                        reservation.getReservationId()
+                        +
+                        "\nRoom ID : "
+                        +
+                        roomId
+        );
+    }
+
+
 }
--- Book_My_Stay/src/main/java/org/example/Main.java
+++ Book_My_Stay/src/main/java/org/example/Main.java
@@ -1,41 +1,183 @@
 package org.example;
+import Entity.Reservation;
+import Service.BookingService;
+import Service.HistoryService;
 import Service.InventoryService;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean flag = true;
-        do{
-            System.out.println("Welcome to the Book My Stay Application");
-            System.out.println("Welcome to the MainMenu \n 1.View Inventory\n 2.Search Room\n 3.Request Booking\n 4.Process Booking\n 5.View Booking History\n 6.Exit\n \n");
-            System.out.print("Enter the choice: ");
-            int choice = input.nextInt();
-            switch (choice)
-            {
-                case 1:
-                    System.out.println("Welcome to the view Inventory");
-                    break;
-                case 2:
-                    System.out.println("Welcome to the search Room");
-                    break;
-                case 3:
-                    System.out.println("Welcome to the Request Booking ");
-                    break;
-                case 4:
-                    System.out.println("Welcome to the Processing Booking ");
-                    break;
-                case 5:
-                    System.out.println("Welcome to the booking history");
-                    break;
-                case 6:
-                    System.out.println("Exiting..........");
-                    flag = false;
-                    break;
-                default:
-                    System.out.println("Invalid choice");
-            }
-        }while(flag);
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        InventoryService inventory =
+                new InventoryService();
+
+        BookingService booking =
+                new BookingService();
+
+        HistoryService history =
+                new HistoryService();
+
+        // Initial Inventory
+
+        inventory.addRoomType("Single", 5, 2500);
+        inventory.addRoomType("Double", 3, 4000);
+        inventory.addRoomType("Suite", 2, 8000);
+
+        Reservation currentReservation = null;
+
+        int choice;
+
+        do {
+
+            System.out.println("\n===== BOOK MY STAY =====");
+
+            System.out.println("1. View Rooms");
+            System.out.println("2. Create Booking");
+            System.out.println("3. Confirm Booking");
+            System.out.println("4. Add Breakfast");
+            System.out.println("5. Add Airport Pickup");
+            System.out.println("6. View Services");
+            System.out.println("7. View Booking History");
+            System.out.println("8. Exit");
+
+            System.out.print("Enter Choice : ");
+
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+
+                case 1:
+
+                    inventory.displayInventory();
+                    break;
+
+                case 2:
+
+                    System.out.print("Customer Name : ");
+                    String customerName =
+                            scanner.nextLine();
+
+                    System.out.print("Room Type (Single/Double/Suite) : ");
+                    String roomType =
+                            scanner.nextLine();
+
+                    currentReservation =
+                            booking.createBooking(
+                                    customerName,
+                                    roomType
+                            );
+
+                    System.out.println(
+                            "Booking Request Added To Queue"
+                    );
+
+                    break;
+
+                case 3:
+
+                    booking.processBooking(
+                            inventory,
+                            history
+                    );
+
+                    break;
+
+                case 4:
+//                    if(currentReservation != null) {
+//
+//                        booking.addService(
+//                                currentReservation,
+//
+//                                new AddOnService(
+//                                        "Breakfast",
+//                                        500
+//                                )
+//                        );
+//
+//
+//                        System.out.println(
+//                                "Breakfast Added"
+//                        );
+//                    }
+//                    else {
+//
+//                        System.out.println(
+//                                "Create Booking First"
+//                        );
+//                    }
+
+                    break;
+
+                case 5:
+
+//                    if(currentReservation != null) {
+//
+//                        booking.addService(
+//                                currentReservation,
+//
+//                                new AddOnService(
+//                                        "Airport Pickup",
+//                                        1000
+//                                )
+//                        );
+//
+//                        System.out.println(
+//                                "Airport Pickup Added"
+//                        );
+//                    }
+//                    else {
+//
+//                        System.out.println(
+//                                "Create Booking First"
+//                        );
+//                    }
+
+                    break;
+
+                case 6:
+
+//                    if(currentReservation != null) {
+//
+//                        booking.showServices(
+//                                currentReservation
+//                        );
+//                    }
+//                    else {
+//
+//                        System.out.println(
+//                                "No Booking Available"
+//                        );
+//                    }
+
+                    break;
+
+                case 7:
+//
+//                    history.showHistory();
+                    break;
+
+                case 8:
+
+                    System.out.println(
+                            "Thank You For Using BookMyStay"
+                    );
+
+                    break;
+
+                default:
+
+                    System.out.println(
+                            "Invalid Choice"
+                    );
+            }
+
+        } while(choice != 8);
+
+        scanner.close();
     }
 }
