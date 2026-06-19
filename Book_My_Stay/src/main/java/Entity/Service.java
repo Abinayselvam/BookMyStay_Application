cf8d2ae8404769640916d1762afc6aea7fd94536
AbinayaSelvam <sabinaya352@gmail.com>
2026-06-19 16:29:44 +0530

    Partical UC2 features

--- Book_My_Stay/src/main/java/Entity/Service.java
+++ Book_My_Stay/src/main/java/Entity/Service.java
@@ -1,4 +1,17 @@
 package Entity;
 
 public class Service {
+    private String serviceName;
+    private double servicePrice;
+    public  Service(String serviceName, double servicePrice) {
+        this.serviceName = serviceName;
+        this.servicePrice = servicePrice;
+    }
+    public String getServiceName() {
+        return serviceName;
+    }
+    public double getServicePrice() {
+        return servicePrice;
+    }
+
 }
